b66e0c3882264c2ae7058001f310977baf8bb3c2
Max Anurin <maxim.anurin@zxteam.net>
2019-04-09 04:37:55 +0300

    Increment version

--- test/general.test.sql
+++ test/general.test.sql
@@ -1,11 +1,11 @@
--- DROP TABLE IF EXISTS tb_1;
-CREATE TABLE tb_1 (
-	varcharValue VARCHAR(128) NOT NULL,
-	intValue     INT          NOT NULL,
-	CONSTRAINT uq_tb_1 UNIQUE (varcharValue),
-	CONSTRAINT uq_tb_1 UNIQUE (intValue)
+-- DROP TABLE IF EXISTS "tb_1";
+CREATE TABLE "tb_1" (
+	"varcharValue" VARCHAR(128) NOT NULL,
+	"intValue"     INT          NOT NULL,
+	CONSTRAINT "uq_tb_1" UNIQUE ("varcharValue"),
+	CONSTRAINT "uq_tb_1" UNIQUE ("intValue")
 );
 
-INSERT INTO tb_1 VALUES ('one', 1);
-INSERT INTO tb_1 VALUES ('two', 2);
-INSERT INTO tb_1 VALUES ('three', 3);
+INSERT INTO "tb_1" VALUES ('one', 1);
+INSERT INTO "tb_1" VALUES ('two', 2);
+INSERT INTO "tb_1" VALUES ('three', 3);
